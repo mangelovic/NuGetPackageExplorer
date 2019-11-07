b308d56dc3a1a1f26dbf5dbc480090205cb68500
Michal Angelovič <Michal.Angelovic@student.tuke.sk>
2019-11-07 16:31:24 +0100

    Created Tests project

---
+++ Tests/NugetPackageExplorer/Converters/BooleanToStringConverterTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PackageExplorer;
+
+namespace Tests
+{
+    [TestClass]
+    public class BooleanToStringConverterTests
+    {
+        [TestMethod]
+        public void Convert_PositiveValue()
+        {
+            var converter = new BooleanToStringConverter();
+
+            var convertedValue = converter.Convert(true, typeof(bool), null, CultureInfo.InvariantCulture);
+
+            Assert.AreEqual("Yes", convertedValue);
+        }
+
+        [TestMethod]
+        public void Convert_NegativeValue()
+        {
+            var converter = new BooleanToStringConverter();
+
+            var convertedValue = converter.Convert(false, typeof(bool), null, CultureInfo.InvariantCulture);
+
+            Assert.AreEqual("No", convertedValue);
+        }
+    }
+}
